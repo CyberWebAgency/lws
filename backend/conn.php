fda83888e50923aaa8032dfea4093149de710324
CyberWebAcency <cyberweb000@gmail.com>
2025-04-02 10:31:13 +0530

    DB conn

--- backend/conn.php
+++ backend/conn.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "lawgical_station");
+$conn = new mysqli("localhost", "xwabppfj_tvl", "Kamal@121", "xwabppfj_lws");
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
